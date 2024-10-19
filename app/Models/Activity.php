dad590bfecfe1207667878961cf09defc6af1224
Sima Bilony <www.simabilony@gmail.com>
2024-10-19 22:39:05 +0300

    Activity

---
+++ app/Models/Activity.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'guide_id',
+        'name',
+        'description',
+        'start_time',
+        'price',
+        'photo'
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function participants()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+}
