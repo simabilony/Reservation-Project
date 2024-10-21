0deb9a00a42bc49eeba75a7da1b1af7b9e430f19
Sima Bilony <www.simabilony@gmail.com>
2024-10-21 14:03:18 +0300

    companies

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    public function index()
+    {
+        $companies = Company::all();
+
+        return view('companies.index', compact('companies'));
+    }
+}
