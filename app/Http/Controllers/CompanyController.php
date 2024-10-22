4a802e9e0f14db136e8066ef70bc62526afe875e
Sima Bilony <www.simabilony@gmail.com>
2024-10-22 23:27:07 +0300

    Create and Edit Companies

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,15 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCompanyRequest;
+use App\Http\Requests\UpdateCompanyRequest;
 use App\Models\Company;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    public function index()
+    public function create()
     {
-        $companies = Company::all();
-
-        return view('companies.index', compact('companies'));
+        return view('companies.create');
     }
+
+    public function store(StoreCompanyRequest $request): RedirectResponse
+    {
+        Company::create($request->validated());
+
+        return to_route('companies.index');
+    }
+
+    public function edit(Company $company)
+    {
+        return view('companies.edit', compact('company'));
+    }
+
+    public function update(UpdateCompanyRequest $request, Company $company): RedirectResponse
+    {
+        $company->update($request->validated());
+
+        return to_route('companies.index');
+    }
+
 }
